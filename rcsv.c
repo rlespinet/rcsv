e561e7faf761ebb3ea6812b0751cb5a29a9d1ad4
Rémi Lespinet <remi@lespi.net>
2018-02-21 23:51:37 +0100

    Add Multi-threading

--- rcsv.c
+++ rcsv.c
@@ -8,8 +8,90 @@
 #include <fcntl.h>
 #include "rcsv.h"
 
-#define BUFFER_SIZE (1L << 16)
+#include "buffer_pool.h"
+#include "thpool.h"
+
+#define BUFFER_SIZE (1L << 20)
 #define INIT_ALLOC  (1L << 16)
+
+// TODO(RL) Modify this!
+#define ALLOC  (1L << 32)
+
+#define NTHREADS 4
+#define BUFFER_POOL_SIZE 16
+
+typedef struct {
+    buffer_pool_t *buff_pool;
+    char *buffer;
+    float *array;
+}  thread_arg_t;
+
+void rcsv_thread_process(void *v_arg) {
+
+    thread_arg_t *arg = (thread_arg_t*) v_arg;
+
+    buffer_pool_t *buff_pool = arg->buff_pool;
+    char* buffer = arg->buffer;
+    float *array = arg->array;
+
+    int array_id = 0;
+
+    char* ptr = buffer;
+    for (;;) {
+
+        char *new_ptr = NULL;
+        float f = strtof(ptr, &new_ptr);
+        /* float f = (float) (*ptr - '0'); */
+        /* new_ptr = (f != 0 && f != 1)?ptr:ptr+1; */
+
+        int read_something = (new_ptr - ptr) != 0;
+
+        while (*new_ptr && *new_ptr == ' ') {
+            (*new_ptr)++;
+        }
+
+        if (*new_ptr && *new_ptr != ',' && *new_ptr != '\n') {
+            read_something = 1;
+            f = NAN;
+
+            while (*new_ptr && *new_ptr != ',' && *new_ptr != '\n') {
+                new_ptr++;
+            }
+        }
+
+        ptr = new_ptr + 1;
+
+        if (read_something) {
+            array[array_id++] = f;
+        }
+
+        if (*new_ptr == '\0') {
+            break;
+        }
+    }
+
+    buffer_pool_release(buff_pool, (void*) buffer);
+
+
+}
+
+char *search_backward(char *start, int size, char c) {
+    char* ptr = start + size - 1;
+    while (ptr >= start && *ptr != c) {
+        ptr--;
+    }
+    return ptr < start ? NULL : ptr;
+}
+
+int count_delim(const char *start, int size) {
+
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (start[i] == ',' || start[i] == '\n')
+            count++;
+    }
+    return count;
+}
 
 int rcsv_read(int *rows, int *cols, float **dest, const char *path) {
 
@@ -20,130 +102,88 @@ int rcsv_read(int *rows, int *cols, float **dest, const char *path) {
         goto clean0;
     }
 
-    char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
-    if (buffer == NULL) {
+    buffer_pool_t buff_pool;
+    int res = buffer_pool_init(&buff_pool, BUFFER_POOL_SIZE, sizeof(char) * BUFFER_SIZE);
+    if (res != 0) {
         goto clean1;
     }
 
-    size_t bytes_left = 0;
+    /* char *buffer = malloc(sizeof(char) * BUFFER_SIZE); */
+    /* if (buffer == NULL) { */
+    /*     goto clean1; */
+    /* } */
 
-    char *pleft = buffer + BUFFER_SIZE;
-    char *pright = NULL;
-
-    int need_reload = 1;
-
-    int rows_count = 0;
-    int elts_count = 0;
-
-    size_t data_capacity = INIT_ALLOC;
-    size_t data_size = 0;
-    float *data = (float*) malloc(sizeof(float) * data_capacity);
-    if (data == NULL) {
+    threadpool pool = thpool_init(NTHREADS - 1);
+    if (pool == NULL) {
         goto clean2;
     }
 
-    int nothing_left = 0;
-    for (;;) {
-
-        if (need_reload) {
-
-            if (nothing_left) {
-                break;
-            }
-
-            if (bytes_left > 0) {
-                memcpy(buffer, buffer + BUFFER_SIZE - bytes_left, bytes_left);
-            }
-
-            ssize_t rd = read(fd, (void*) (buffer + bytes_left), BUFFER_SIZE - bytes_left);
-            if (rd < 0) {
-                goto clean3;
-            }
-
-            if (rd < BUFFER_SIZE - bytes_left) {
-                nothing_left = 1;
-                buffer[bytes_left + rd] = '\0';
-            } else {
-
-                char *end_ptr = buffer + BUFFER_SIZE - 1;
-                while (end_ptr >= buffer && *end_ptr != ',') {
-                    end_ptr--;
-                }
-                if (end_ptr < buffer) {
-                    goto clean3;
-                }
-                *end_ptr = '\0';
-
-                bytes_left = buffer + BUFFER_SIZE - end_ptr - 1;
-            }
-
-            pleft = buffer;
-            need_reload = 0;
-
-        }
-        /* float f = strtof(pleft, &pright); */
-        float f = (float) (*pleft - '0');
-        pright = (f != 0 && f != 1)?pleft:pleft+1;
-
-        int valid_f = (pright - pleft > 0);
-
-        int invalid_chars = 0;
-        for (;;) {
-            if (*pright == '\0') {
-                need_reload = 1;
-                break;
-            } else if (*pright == '\n') {
-                rows_count++;
-                /* printf("%d\n", rows_count); */
-                break;
-            } else if (*pright == ',') {
-                elts_count++;
-                break;
-            } else if (!isspace(*pright)) {
-                f = NAN;
-                valid_f = 1;
-            }
-
-            pright++;
-        }
-
-        if (valid_f) {
-
-            if (data_size + 1 > data_capacity) {
-
-                size_t new_data_capacity = data_capacity * 2;
-                float *new_data = (float*) realloc(data, sizeof(float) * new_data_capacity);
-                if (new_data == NULL) {
-                    goto clean3;
-                }
-
-                data = new_data;
-                data_capacity = new_data_capacity;
-            }
-
-            data[data_size++] = f;
-        }
-
-        pleft = pright + 1;
-
-    }
-
-    if (data_size % rows_count != 0) {
-        // Bad read
+    size_t data_size = 0;
+    float *data = (float*) malloc(sizeof(float) * ALLOC);
+    if (data == NULL) {
         goto clean3;
     }
 
-    *rows = rows_count;
-    *cols = data_size / rows_count;
+    char *buffer = (char*) buffer_pool_get(&buff_pool);
+    size_t bytes_left = 0;
+
+    int finished = 0;
+    while (!finished) {
+
+        ssize_t rd = read(fd, (void*) (buffer + bytes_left), BUFFER_SIZE - bytes_left);
+        if (rd < 0) {
+            goto clean4;
+        }
+
+        char *next_buffer = NULL;
+        char *split = NULL;
+        if (rd < BUFFER_SIZE - bytes_left) {
+            split = buffer + bytes_left + rd;
+            bytes_left = 0;
+            finished = 1;
+        } else {
+            split = search_backward(buffer, BUFFER_SIZE, ',');
+            bytes_left = buffer + BUFFER_SIZE - split + 1;
+            next_buffer = (char*) buffer_pool_get(&buff_pool);
+            memcpy(next_buffer, buffer + BUFFER_SIZE - bytes_left, bytes_left);
+        }
+
+        if (split == NULL) {
+            goto clean4;
+        }
+
+        *split = '\0';
+
+        thread_arg_t arg = {&buff_pool, buffer, data + data_size};
+
+        int w = thpool_add_work(pool, rcsv_thread_process, (void*) &arg);
+        if (w != 0) {
+            goto clean4;
+        }
+
+        int delim = count_delim(buffer, split - buffer);
+        data_size += delim + 1;
+
+        buffer = next_buffer;
+
+    }
+
+    thpool_wait(pool);
 
     *dest = data;
-    ret = 0;
-    goto clean2;
+    *rows = 1;
+    *cols = data_size;
 
-clean3:
+
+    ret = 0;
+    goto clean3;
+
+clean4:
     free(data);
+clean3:
+    thpool_destroy(pool);
 clean2:
-    free(buffer);
+    buffer_pool_term(&buff_pool);
 clean1:
     close(fd);
 clean0:
