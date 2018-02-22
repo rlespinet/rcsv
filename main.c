4a1b41796aa53a98a83e175e267edf3a626b5693
Rémi Lespinet <remi@lespi.net>
2018-02-22 01:36:29 +0100

    Modify main

--- main.c
+++ main.c
@@ -2,24 +2,15 @@
 #include <stdlib.h>
 #include "rcsv.h"
 
-
-
 int main(int argc, char **argv) {
 
     int rows;
     int cols;
     float *data;
 
-    rcsv_read(&rows, &cols, &data, "test_big.csv");
-
-    /* for (int i = 0; i < cols; i++) { */
-    /*     printf("%f\n", data[i]); */
-    /* } */
+    rcsv_read(&rows, &cols, &data, "data/full.csv");
 
     printf("%d, %d\n", rows, cols);
-    for (int i = cols; i < 2*cols; i++) {
-        printf("%f ", data[i]);
-    }
 
     free(data);
 
